5a26a9872b5004a2567e949ad5f5fbd9870daf66
Leonardo Santiago <leocsdcc@gmail.com>
2016-10-25 17:26:54 -0200

    pequena correcao

--- conta/login.php
+++ conta/login.php
@@ -5,7 +5,7 @@ include_once("../core/templates/cabecalho.php");
     <div class="col-sm-6 col-md-4 col-md-offset-4">
         <form class="form-signin" role="form" method="post" action="verificaLogin.php">
             <h3 class="form-signin-heading">Learn Alone : Login</h3>
-            <input type="text" class="form-control" placeholder="Login" name="login" required autofocus>
+            <input type="text" class="form-control" placeholder="Login" name="login" pattern="[a-zA-Z0-9]+" required autofocus>
             <input type="password" class="form-control" placeholder="Senha" name="passwd" required>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
             <br>
